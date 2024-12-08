e26342d7f37cccd9b82276f83c5690895d89ceb6
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2024-12-08 17:22:58 +0000

    Initailize tailwind Setup the Routing of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/collection' element={<Collection/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/place-order' element={<PlaceOrder/>}/>
+          <Route path='/product/:productId' element={<Product/>}/>
+          <Route path='cart' element={<Cart/>}/>
+          <Route path='orders' element={<Orders/>}/>
+          <Route path='collection' element={<Collection/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
